7309626269cb96b1a2be2da67b2b6f2460abdd29
Maia-Francine-Endaya <88295922+Maia-Francine-Endaya@users.noreply.github.com>
2024-02-22 09:13:38 +0800

    Asteroids added

---
+++ AstroPong/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+  //Will add behavior codes later
+  //Referencing this: https://www.youtube.com/watch?v=wzQ9Xn406wc
+
+  //Set starting asteroid size
+  public int size = 3;
+
+  private void Start()
+  {
+  }
+}
