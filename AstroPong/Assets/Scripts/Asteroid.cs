e693f363e2538c2b36e20ce6a863bfdfeda49edf
Maia-Francine-Endaya <88295922+Maia-Francine-Endaya@users.noreply.github.com>
2024-02-24 20:23:31 +0800

    Added asteroid movement

--- AstroPong/Assets/Scripts/Asteroid.cs
+++ AstroPong/Assets/Scripts/Asteroid.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 
+//NOTE: System.Numerics is removed for Vector2 lines of code to function
 public class Asteroid : MonoBehaviour
 {
-  //Will add behavior codes later
   //Referencing this: https://www.youtube.com/watch?v=wzQ9Xn406wc
-
   //Set starting asteroid size
   public int size = 3;
 
+
   private void Start()
   {
+    //Scale based on the size
+    transform.localScale = 0.5f * size * Vector3.one;
+
+    //Applies Movement, bigger = slower smaller = faster
+    Rigidbody2D rb = GetComponent<Rigidbody2D>();
+    Vector2 direction = new Vector2(Random.value, Random.value).normalized;
+    float spawnSpeed = Random.Range(4f - size, 5f - size);
+    rb.AddForce(direction * spawnSpeed, ForceMode2D.Impulse);
+
+    //Register creation
+    // GameManager.asteroidCount += 1;
   }
+
 }
--- AstroPong/Assets/Scripts/GameManager.cs
+++ AstroPong/Assets/Scripts/GameManager.cs
@@ -3,46 +3,89 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
+
+//NOTE: System.Numerics is removed for Vector2 lines of code to function
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    public TMP_Text scoreText;
-    public static int score = 0;
-    public static int highScore;
-    
-    [SerializeField]
-    public static int playerHealth = 3;
-    public Image[] playerLives;
-    public Sprite fullLives;
-    public Sprite emptyLives;
-    // Start is called before the first frame update
-    void Start()
+  [SerializeField]
+  public TMP_Text scoreText;
+  public static int score = 0;
+  public static int highScore;
+
+  // //New code will be here
+  // public int asteroidCount = 0;
+  // private int level = 0;
+  // [SerializeField] private Asteroid asteroidPrefab;
+
+  [SerializeField]
+  public static int playerHealth = 3;
+  public Image[] playerLives;
+  public Sprite fullLives;
+  public Sprite emptyLives;
+
+  void Update()
+  {
+    scoreText.SetText(score.ToString());
+    PlayerHealth();
+
+    if (score > highScore)
     {
-        
+      highScore = score;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        scoreText.SetText(score.ToString());
-        PlayerHealth();
+    // //Modifications will start here
+    // if (asteroidCount == 0)
+    // {
+    //   level++;
+    // }
 
-        if (score > highScore)
-        {
-            highScore = score;
-        }
+    // int numAsteroids = 2 + (2 * level);
+    // for (int i = 0; i < numAsteroids; i++)
+    // {
+    //   SpawnAsteroid();
+    // }
+  }
+
+  private void PlayerHealth()
+  {
+    foreach (Image img in playerLives)
+    {
+      img.sprite = emptyLives;
     }
 
-    private void PlayerHealth()
+    for (int i = 0; i < playerHealth; i++)
     {
-        foreach (Image img in playerLives)
-        {
-            img.sprite = emptyLives;
-        }
-
-        for(int i=0; i < playerHealth; i++)
-        {
-            playerLives[i].sprite = fullLives;
-        }
+      playerLives[i].sprite = fullLives;
     }
+  }
+
+  //spawn asteroid method will be here
+  // private void SpawnAsteroid()
+  // {
+  //   float offset = Random.Range(0f, 1f);
+  //   Vector2 viewportSpawnPosition = Vector2.zero;
+
+  //   //Which edge
+  //   int edge = Random.Range(0, 4);
+  //   if (edge == 0)
+  //   {
+  //     viewportSpawnPosition = new Vector2(offset, 0);
+  //   }
+  //   else if (edge == 1)
+  //   {
+  //     viewportSpawnPosition = new Vector2(offset, 1);
+  //   }
+  //   else if (edge == 2)
+  //   {
+  //     viewportSpawnPosition = new Vector2(0, offset);
+  //   }
+
+  // Creates the asteroid
+  // Vector2 worldSpawnPosition = Camera.main.ViewportToWorldPoint(viewportSpawnPosition);
+  // Asteroid asteroid = Instantiate(asteroidPrefab, worldSpawnPosition, Quaternion.identity);
+  // asteroid.gameManager = this;
+
+
+  // }
 }
